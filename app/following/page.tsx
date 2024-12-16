35110d267d84103d96d16d5d20128fa3ad004730
Rocky Jay <rockyjay_g@yahoo.com>
2024-12-16 02:54:05 -0800

    adding new style

--- app/following/page.tsx
+++ app/following/page.tsx
@@ -3,10 +3,21 @@ import ActiveLink from "@/components/ActiveLink";
 export default function Home() {
   return (
     <section className=" flex flex-col w-full">
+      <div className=" hidden sm:flex items-center gap-2 h-20 w-full  px-4 border-b border-stone-300">
+        <div className=" h-10 w-10 bg-black/30 rounded-full"></div>
+        <input
+          type="text"
+          placeholder="What's new?"
+          className=" placeholder:text-sm placeholder:font-semibold placeholder:text-stone-400/85"
+        />
+        <button className=" text-black ml-auto py-1 px-4 border border-stone-300 rounded-lg">
+          Post
+        </button>
+      </div>
       <div className=" w-full grid grid-cols-2">
         <ActiveLink
           href="/"
-          className="h-12 flex items-center justify-center border-b font-semibold"
+          className="h-12 flex items-center justify-center border-b  font-semibold"
         >
           For you
         </ActiveLink>
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,17 @@ import ActiveLink from "@/components/ActiveLink";
 export default function Home() {
   return (
     <section className=" flex flex-col w-full">
-      <div className=" hidden sm:block h-14 w-full"></div>
+      <div className=" hidden sm:flex items-center gap-2 h-20 w-full  px-4 border-b border-stone-300">
+        <div className=" h-10 w-10 bg-black/30 rounded-full"></div>
+        <input
+          type="text"
+          placeholder="What's new?"
+          className=" placeholder:text-sm placeholder:font-semibold placeholder:text-stone-400/85"
+        />
+        <button className=" text-black ml-auto py-1 px-4 border border-stone-300 rounded-lg">
+          Post
+        </button>
+      </div>
       <div className=" w-full grid grid-cols-2">
         <ActiveLink
           href="/"
