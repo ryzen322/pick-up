be8b09b75bd595c9a607a9328951986e85b3a046
Rocky Jay <rockyjay_g@yahoo.com>
2024-12-15 20:56:59 -0800

    fix the auth

--- auth.ts
+++ auth.ts
@@ -1,10 +1,21 @@
 import NextAuth from "next-auth";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
-import { db } from "./server/schema";
+import { db } from "./server/db";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: DrizzleAdapter(db),
   secret: process.env.AUTH_SECRET!,
   session: { strategy: "jwt" },
-  providers: [],
+  providers: [
+    Credentials({}),
+    GitHub({
+      allowDangerousEmailAccountLinking: true,
+    }),
+    Google({
+      allowDangerousEmailAccountLinking: true,
+    }),
+  ],
 });
