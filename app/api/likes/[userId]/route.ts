b80c2c2f217e99985dee5bf4b7f4f4189751dcf2
Rocky Jay <rockyjay_g@yahoo.com>
2024-12-23 03:39:55 -0800

    redeploy

--- app/api/likes/[userId]/route.ts
+++ app/api/likes/[userId]/route.ts
@@ -1,11 +1,10 @@
 import { getCascheLikes } from "@/server/queries";
 import { NextResponse } from "next/server";
 
-export async function GET(
-  request: Request,
-  { params }: { params: { userId: string } }
-) {
-  const userId = params.userId;
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const userId = searchParams.get("userId");
+
   try {
     const likes = await getCascheLikes(Number(userId));
 
