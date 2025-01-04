4937ce981e88a38dce7961345d8398b73c30cfd9
rocky jay <rockyjay_g@yahoo.com>
2025-01-03 20:20:15 -0800

    redeploy

--- components/PostIcon.tsx
+++ components/PostIcon.tsx
@@ -4,7 +4,9 @@ import { DrawerDialogDemo } from "./PostDrawer";
 const PostIcon = () => {
   return (
     <div className=" rounded-md flex items-center justify-center  transition-all duration-200 ">
-      <DrawerDialogDemo />
+      <DrawerDialogDemo>
+        <div className=""></div>
+      </DrawerDialogDemo>
     </div>
   );
 };
